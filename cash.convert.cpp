1ad575a2f1177a7d62f60a59045b29c7743f0870
The Entelechy <futa.henona@protonmail.com>
2025-02-07 11:46:46 -0800

    create currency tracker using pointers and add running total to usdBank structure

--- cash.convert.cpp
+++ cash.convert.cpp
@@ -4,7 +4,6 @@
 struct foreignCurrency {
   std::string name;
   double currency;
-  double convertedCurr;
   double conversionRate;
   foreignCurrency* ptr = nullptr;
 };
@@ -12,18 +11,23 @@ struct foreignCurrency {
 struct usdBank{
   double usd {0.00};
 };
-void currencyTracker(foreignCurrency &c, usdBank &b){
+void currencyTracker(std::vector<foreignCurrency*>* ptr, usdBank &b){
 // Prompt currency 
 // Enter currency for each
 // output total USD after converted
-  std::cout << "Enter number of " << c.name << ":";
-  std::cin >> c.currency;
-  std::cout << "You Entered " << c.currency << " " << c.name << '\n';
-  c.convertedCurr = c.currency * c.conversionRate;
-  std::cout << "This is: $" << c.convertedCurr << '\n';
-  b.usd += c.convertedCurr;
-  std::cout << &c << '\n';
-  std::cout << "Total: $" << b.usd << '\n';
+  double convertedCurr = 0;
+  for(int i=0;i < ptr->size();i++){
+    //std::cout << (*ptr)[i]->name << '\n';
+    std::cout << "Enter number of " << (*ptr)[i]->name << ":";
+    std::cin >> (*ptr)[i]->currency;
+    std::cout << "You Entered " << (*ptr)[i]->currency << " " << (*ptr)[i]->name << '\n';
+    convertedCurr = (*ptr)[i]->currency * (*ptr)[i]->conversionRate;
+    std::cout << "This is: $" << convertedCurr << '\n';
+    b.usd += convertedCurr;
+    std::cout << "Total: $" << b.usd << '\n';
+  };
+    
+  
 };
 
 //get struct,name,currency
@@ -32,7 +36,7 @@ void currencyCreate(foreignCurrency &c, std::string n, double conv, std::vector<
   c.conversionRate = conv;
   c.ptr = &c;
   ptr->push_back(c.ptr);
-  std::cout << ptr->size() << '\n';
+  //std::cout << ptr->size() << '\n';
 
   //std::cout << c.name <<'\n' << &c << '\n'; 
 };
@@ -41,12 +45,14 @@ int main(){
   bool banking = true;
   foreignCurrency Colombia, Brazil, Peru;
   std::vector<foreignCurrency*> V;
+  std::vector<foreignCurrency*>* ptr = &V;
   usdBank myBank;
-  currencyCreate(Colombia, "Colombian Peso", 0.00024, &V);
-  //currencyCreate(Brazil, "Brazilian Reais",0.17);
-  //currencyCreate(Peru, "Peruvian Soles",0.27);
-  std::cout << V[0]->name << '\n';
-  
+  currencyCreate(Colombia, "Colombian Peso", 0.00024, ptr);
+  currencyCreate(Brazil, "Brazilian Reais",0.17, ptr);
+  currencyCreate(Peru, "Peruvian Soles",0.27, ptr);
+  //std::cout << V.size() << '\n';
+
+  currencyTracker(ptr,myBank);
   //std::cout << myBank.usd;
   //std::cout << &Brazil << " in Main"; //address matches inside function
 
